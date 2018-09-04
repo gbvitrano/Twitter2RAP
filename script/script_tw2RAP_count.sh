bffa5b8bbc894ba150932f983345ad44f8cf011f
Andrea Borruso <aborruso@gmail.com>
2018-09-04 17:42:30 +0200

    Update script_tw2RAP_count.sh

--- script/script_tw2RAP_count.sh
+++ script/script_tw2RAP_count.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
 
-ogr2ogr -f CSV tagsOutCount.CSV -dialect sqlite quartieri_pa_4326.sqlite -sql "SELECT date('now')as data,QUA_ID, count(*) as nro FROM tags t, quartieri q WHERE st_contains (q.geometry,t.geometry) GROUP BY 2";
+ogr2ogr -f CSV tagsOutCount.CSV -dialect sqlite quartieri_pa_4326.sqlite \
+-sql "SELECT date('now')as data,QUA_ID, count(*) as nro FROM tags t, quartieri q WHERE st_contains (q.geometry,t.geometry) GROUP BY 2";
 
