c3fac1df95bf54768042a54859e5dab235f58d42
Salvatore Fiandaca <pigrecoinfinito@gmail.com>
2018-09-04 17:32:57 +0200

    add script sh

---
+++ script/script_tw2RAP_count.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+
+ogr2ogr -f CSV tagsOut.CSV -dialect sqlite quartieri_pa_4326.sqlite -sql "SELECT date('now')as data,QUA_ID, count(*) as nro FROM tags t, quartieri q WHERE st_contains (q.geometry,t.geometry) GROUP BY 2";
+
