3ecc38ac5f537f7f0996741945ae0cb6baae9789
aborruso <aborruso@gmail.com>
2018-09-05 21:52:35 +0200

    aggiunti dati rap

---
+++ script/README.md
@@ -0,0 +1,11 @@
+# Intro
+
+Ecco alcuni script usati in questo progetto.
+
+## rap.sh
+
+Uno script che recupera le segnalazioni fatte a RAP, usando l'app pubblicata dall'azienda, a partire dal primo gennaio 2018.
+
+I dati sono estratti da [questa pagina](http://www.rapspaservizi.it/develop1/bastaundito/bastaundito_dev/mappa) e aggiornati settimanalmente.
+
+L'output grezzo dello script è qui [https://query.data.world/s/537rnrly6i5sl6p5itgzlpn4ohi3ul](https://query.data.world/s/537rnrly6i5sl6p5itgzlpn4ohi3ul)
---
+++ script/rap.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -x
+
+cartella="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# leggo la data di oggi
+oggi=$(echo $(date +'%d %m %Y') | sed 's/ /%2F/g')
+# Prima scarico i dati
+# le segnalazioni aperte
+curl -sL 'http://www.rapspaservizi.it/develop1/bastaundito/bastaundito_dev/mappa' -H 'Connection: keep-alive' \
+-H 'Cache-Control: max-age=0' -H 'Origin: http://www.rapspaservizi.it' -H 'Upgrade-Insecure-Requests: 1' \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.40 Safari/537.36' \
+-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' \
+-H 'Referer: http://www.rapspaservizi.it/develop1/bastaundito/bastaundito_dev/mappa' -H 'Accept-Encoding: gzip, deflate' \
+-H 'Accept-Language: it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7' \
+--data 'giorno_dal=01%2F01%2F2018&giorno_al='"$oggi"'&attivita_id=31&circoscrizione_id=T&stato=A' >"$cartella"/aperte
+# le segnalazioni aperte
+curl -sL 'http://www.rapspaservizi.it/develop1/bastaundito/bastaundito_dev/mappa' -H 'Connection: keep-alive' \
+-H 'Cache-Control: max-age=0' -H 'Origin: http://www.rapspaservizi.it' -H 'Upgrade-Insecure-Requests: 1' \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.40 Safari/537.36' \
+-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' \
+-H 'Referer: http://www.rapspaservizi.it/develop1/bastaundito/bastaundito_dev/mappa' -H 'Accept-Encoding: gzip, deflate' \
+-H 'Accept-Language: it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7' \
+--data 'giorno_dal=01%2F01%2F2018&giorno_al='"$oggi"'&attivita_id=31&circoscrizione_id=T&stato=C' >"$cartella"/chiuse
+
+
+# Le info anagrafiche su ogni "pallino" (le coordinate sono in un blocco diverso da quello anagrafico)
+## creo riga di intestazione
+echo "data,indirizzo,foto,stato" >"$cartella"/dati.csv
+## estraggo info su segnalazioni aperte
+<"$cartella"/aperte grep -P 'marker_[0-9]+.set.*' | \
+perl -pe  's|(marker_)([0-9]+)(.*?)(<h[0-9]>)(.*?)(<.h[0-9]>)(<p>)(.*?)(<.p>)(.*?)(src=.")(.*?)(.")(.*)$|$5#$8#$12|g' | \
+grep -v 'marker' | csvformat -d "#" | sed -e 's/$/,aperta/g' | sed -r 's/^\s+//' >>"$cartella"/dati.csv
+## estraggo info su segnalazioni aperte chiuse
+<"$cartella"/chiuse grep -P 'marker_[0-9]+.set.*' | \
+perl -pe  's|(marker_)([0-9]+)(.*?)(<h[0-9]>)(.*?)(<.h[0-9]>)(<p>)(.*?)(<.p>)(.*?)(src=.")(.*?)(.")(.*)$|$5#$8#$12|g' | \
+grep -v 'marker' | csvformat -d "#" | sed -e 's/$/,chiusa/g' | sed -r 's/^\s+//' >>"$cartella"/dati.csv
+
+## info sulle coordinate
+echo "latitude,longitude" >"$cartella"/coords.txt
+<"$cartella"/aperte grep -P 'google.maps.LatLng' | tail -n +3 | sed -r 's/^\s+//g' | perl -pe  's/^(.+?\()(.+?)(.;)/$2/g' >>"$cartella"/coords.txt
+<"$cartella"/chiuse grep -P 'google.maps.LatLng' | tail -n +3 | sed -r 's/^\s+//g' | perl -pe  's/^(.+?\()(.+?)(.;)/$2/g' >>"$cartella"/coords.txt
+# metto insieme le info anagrafiche con quelle spaziali
+paste -d , dati.csv "$cartella"/coords.txt >"$cartella"/output.csv
+
+
+# rimuovo i record che hanno 0 come coordinate e creo geojson
+<"$cartella"/output.csv csvgrep -c "latitude,longitude" -r '^.{0,1}0.{0,1}$' -i | csvjson --lat latitude --lon longitude >"$cartella"/output.geojson
+
+mv "$cartella"/output.csv "$cartella"/segnalazioniAppRAP.csv
+
+
+# faccio l'upload su data.world (leggo la API KEY da config.txt)
+source "$cartella"/config.txt
+curl "https://api.data.world/v0/uploads/opendatasicilia/tw2rap/files" -F file=@"$cartella"/segnalazioniAppRAP.csv -H "Authorization: Bearer ${DW_API_TOKEN}"
