18f5ae7c8b498c534b150772efeb1927f8c60b37
Salvatore Fiandaca <pigrecoinfinito@gmail.com>
2018-09-06 15:41:05 +0200

    add commento source

--- script/script_tw2RAP_count.sh
+++ script/script_tw2RAP_count.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
+# source URL csv: https://query.data.world/s/c2a64w2ce4pgyzy6xrfgpneiz3vi4k
 ogr2ogr -f CSV tagsOutCount.CSV -dialect sqlite quartieri_pa_4326.sqlite \
 -sql "SELECT date('now')as data,QUA_ID, count(*) as nro FROM tags t, quartieri q WHERE st_contains (q.geometry,t.geometry) GROUP BY 2";
 
